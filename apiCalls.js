a9addc6a3a77d6abb887f3fdbd3dd1eed73f062d
Adrian Jost <adrianjost@users.noreply.github.com>
2019-03-27 20:49:14 +0100

    Update apiCalls.js

--- apiCalls.js
+++ apiCalls.js
@@ -2,7 +2,7 @@ import fetch from "isomorphic-unfetch";
 import base64Img from "base64-img";
 
 const apiBaseUrl =
-	process.env.NODE_ENV === "production"
+	process.env.NODE_ENV !== "development"
 		? "https://us-central1-curriculum-vitae-5cd0a.cloudfunctions.net/fastApiProjects"
 		: "http://localhost:5000/curriculum-vitae-5cd0a/us-central1/fastApiProjects";
 
