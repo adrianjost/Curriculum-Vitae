ea7aae82255a1935ee3bf7ca3b6520b81f97e4fb
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-09-27 11:43:41 +0200

    add more traces to perf monitoring

--- src/plugins/performance.js
+++ src/plugins/performance.js
@@ -1,6 +1,20 @@
 import { firebase } from "~/plugins/firebase.js";
 import "firebase/performance";
 
+const enablePerfMetrics = () => {
+	const perf = firebase.performance();
+
+	const trace = perf.trace("UserEvents");
+	document.addEventListener("mousedown", () => {
+		trace.incrementMetric("Mouse Clicks", 1);
+	});
+	document.addEventListener("keydown", (evt) => {
+		if (!["TEXTAREA", "INPUT"].includes(evt.target.nodeName)) {
+			trace.incrementMetric("Key Presses", 1);
+		}
+	});
+};
+
 if (process.env.NODE_ENV === "production") {
-	firebase.performance();
+	enablePerfMetrics();
 }
