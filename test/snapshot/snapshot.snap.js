e32075270120cc9d8c917e1444e7eaccb935e84e
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-04-28 22:14:23 +0200

    implement snapshot testing of rendered content

---
+++ test/snapshot/snapshot.snap.js
@@ -0,0 +1,480 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`Homepage "/" should match snapshot 1`] = `
+"Adrian Jost
+ProjectsAboutContact
+About Me
+
+I strive to develop software that is accessible to everyone.
+
+That's why I have specialized in web development and put special emphasis on accessibility, usability and performance. However, the speed of development should not suffer, which is why I love to automate things and try out new supporting tools whenever possible.
+
+Even though I am young I already have over 10 years of coding experience and been building websites for over 5 years now.
+
+Learning new things every day is for me the key to developing the best software available! You don't believe me? Check out all my projects ;)
+
+In my spare time, I like to ride and repair my mountain- and road bike 🚲.
+
+My Skills
+My Employers
+HPI Schul-Cloud
+02.2017 - 03.2020 Web Developer
+
+My first task was to make minor UI adjustments and design the UI of new tools, but also to implement it. As time went by I was assigned more and more responsibilities and now I am one of the longest active developers in the project. Sadly, the development team changed a couple of times in the last 2 years, which resulted in some really bad code and architectures. For this reason I was allowed to invest more and more time in the documentation and onboarding of new developers and I am now one of the lead developers in the complete rewrite of the frontend.  My task is to integrate the learnings, from the first version, into the second version and prevent the failures we had done before. This means that I develop a new architecture for the project together with a small team,  I integrate workflow supporting tools (e.g. for documentation or git workflows) and develop concepts on how to optimize our code quality and test coverage.
+
+Lightball SC
+10.2016 - 01.2017 Angular App Developer
+
+I integrated Wikipedia and Youtube into the \\"Quissen\\" Android App and implemented some UI and UX improvements. I also adjusted some backend data structures for new features, like scheduled publishing. The techstack included Angular 1 with the Framework Ionic for the App, and Wordpress as a CMS with some Plugins to serve the Content via REST to the App.
+
+My Education
+Hasso-Plattner Institut (Potsdam, GER)
+10.2016 - 03.2020 IT-Systems Engineering (B. Sc.)
+Archenhold Gymnasium (Berlin, GER)
+08.2010 - 06.2016 A level
+ Projects
+Contact Me "
+`;
+
+exports[`Homepage "/about" should match snapshot 1`] = `
+"Adrian Jost
+ProjectsAboutContact
+About Me
+
+I strive to develop software that is accessible to everyone.
+
+That's why I have specialized in web development and put special emphasis on accessibility, usability and performance. However, the speed of development should not suffer, which is why I love to automate things and try out new supporting tools whenever possible.
+
+Even though I am young I already have over 10 years of coding experience and been building websites for over 5 years now.
+
+Learning new things every day is for me the key to developing the best software available! You don't believe me? Check out all my projects ;)
+
+In my spare time, I like to ride and repair my mountain- and road bike 🚲.
+
+My Skills
+My Employers
+HPI Schul-Cloud
+02.2017 - 03.2020 Web Developer
+
+My first task was to make minor UI adjustments and design the UI of new tools, but also to implement it. As time went by I was assigned more and more responsibilities and now I am one of the longest active developers in the project. Sadly, the development team changed a couple of times in the last 2 years, which resulted in some really bad code and architectures. For this reason I was allowed to invest more and more time in the documentation and onboarding of new developers and I am now one of the lead developers in the complete rewrite of the frontend.  My task is to integrate the learnings, from the first version, into the second version and prevent the failures we had done before. This means that I develop a new architecture for the project together with a small team,  I integrate workflow supporting tools (e.g. for documentation or git workflows) and develop concepts on how to optimize our code quality and test coverage.
+
+Lightball SC
+10.2016 - 01.2017 Angular App Developer
+
+I integrated Wikipedia and Youtube into the \\"Quissen\\" Android App and implemented some UI and UX improvements. I also adjusted some backend data structures for new features, like scheduled publishing. The techstack included Angular 1 with the Framework Ionic for the App, and Wordpress as a CMS with some Plugins to serve the Content via REST to the App.
+
+My Education
+Hasso-Plattner Institut (Potsdam, GER)
+10.2016 - 03.2020 IT-Systems Engineering (B. Sc.)
+Archenhold Gymnasium (Berlin, GER)
+08.2010 - 06.2016 A level
+ Projects
+Contact Me "
+`;
+
+exports[`Homepage "/contact" should match snapshot 1`] = `
+"Adrian Jost
+ProjectsAboutContact
+Contact Me
+
+My E-Mail: me@adrianjost.dev 
+
+Your E-Mail
+Your Message
+SEND MESSAGE →
+ About Me"
+`;
+
+exports[`Homepage "/projects" should match snapshot 1`] = `
+"Adrian Jost
+ProjectsAboutContact
+click to zoom
+Surge.Sh Teardown GitHub Action
+Work - December 2019
+
+At work we  deploy all pull requests to surge.sh. To reduce the amount of deployed pages I developed this GitHub Action to tear down all projects that match a RegEx
+
+Used Tools:
+JAVASCRIPT
+GITHUB ACTIONS
+Links:
+click to zoom
+SmartLight V3
+Hobby - November 2019
+
+Version 3 of my own Smart Home Lighting System is featuring gradient support for using it as a light alert in the morning. It is planned to be compatible with a wide variaty of devices and supports all features of the previous versions plus a beautiful new minimalistic ui.
+
+Used Tools:
+GIT
+HTML5
+VUE.JS
+FIREBASE
+CSS3
+JAVASCRIPT
+ARDUINO
+TRAVIS CI
+VUEX
+EASYEDA
+SOLDERING
+Links:
+READ MORE →
+click to zoom
+Two Channel Picker
+Hobby - November 2019
+
+As part of SmartLight V3 I developed a two channel color picker that I also published on npmjs.org.
+
+Used Tools:
+VUE.JS
+JAVASCRIPT
+GIT
+CSS3
+HTML5
+Links:
+click to zoom
+Markdown 2 Latex Converter
+Hobby - August 2019
+
+I was to lazy to write my bachelor thesis with Latex, so I've build a Markdown to Latex Converter 😅.
+
+Used Tools:
+JAVASCRIPT
+NODE.JS
+GIT
+Links:
+click to zoom
+Vue Filter Component
+Work - July 2019
+
+Vue Filter Component for FeathersJS inspired by the UI of Googles Webmaster Console. The complete UI is exchangeable and it offers an API to customize the output query for different databases.
+
+Used Tools:
+VUE.JS
+CSS3
+JAVASCRIPT
+GIT
+FEATHERSJS
+MONGODB
+Links:
+READ MORE →
+click to zoom
+Dedent Tabs
+Hobby - June 2019
+
+The package dmnd/dedent is pretty awesome, but it is lacking support for tab indentation and is not maintained anymore. For that reason I continued maintaing it and published dedent-tabs wich is now downloaded over 10.000x per Week.
+
+Used Tools:
+NODE.JS
+JAVASCRIPT
+Links:
+click to zoom
+Slate.JS Sorting Task Plugin
+University - February 2019
+
+As part of the course \\"Web Development with Web Frameworks\\" we have developed a Slate.JS plugin, which will transfer the educational concept of sorting tasks into the digital world. We focused our development on a super simple, intuitive design for students and teachers. In addition, we focused on writing really clean code to ensure a smooth transition to the core editor development team.
+
+Used Tools:
+REACT
+JAVASCRIPT
+CSS3
+HTML5
+BEM
+SLATE.JS
+GIT
+Links:
+click to zoom
+Nextcloud Metadata Plugin
+University - January 2019
+
+For research, my fellow students and I implemented an prototype of an Nextcloud Plugin that can display additional, externaly saved metadata for files.
+
+Used Tools:
+NEXTCLOUD
+PHP
+JAVASCRIPT
+GIT
+Links:
+click to zoom
+Console TicTacToe
+Hobby - November 2018
+
+Just for fun, I created a basic TicTacToe game that you can run in your browsers console.
+
+Used Tools:
+JAVASCRIPT
+GIT
+Links:
+click to zoom
+Modular Text Editor
+University - July 2018
+
+I think Smalltalk is a dead language, but we got the task to develop a component-based, modular editor, similar to the editors of Gitbook.io and Wordpress (Gutenberg). We have done our best to ensure good usability.
+
+Used Tools:
+SQUEAK
+SMALLTALK
+GIT
+SCRUM
+TRAVIS CI
+COVERALLS.IO
+Links:
+click to zoom
+Metadaten Editor
+University - June 2018
+
+In the seminar \\"Web Programming and Web Frameworks\\" we developed a content management software for teaching content.
+
+Used Tools:
+VUE.JS
+GIT
+JAVASCRIPT
+CSS3
+HTML5
+FEATHERSJS
+TRAVIS CI
+Links:
+click to zoom
+Robot Competition
+University - May 2018
+
+We had many really cheap electronic components, a piece of plywood and the task of building a robot that can navigate to a destination with simple light sensors. Our robot was one of only two robots that could move and the only one that could find the target, so we won this competition.
+
+Used Tools:
+ARDUINO
+LASER CUTTER
+SOLDERING
+Links:
+READ MORE →
+click to zoom
+SmartLight V2
+Hobby - April 2018
+
+Version 2
+
+Used Tools:
+JAVASCRIPT
+VUE.JS
+FIREBASE
+GIT
+Links:
+READ MORE →
+click to zoom
+SmartLight V1
+Hobby - September 2017
+
+My own smart home lighting system. I designed it with the goal of a simple setup process, an frictionless UI and very cheap hardware.
+
+Used Tools:
+JAVASCRIPT
+ARDUINO
+HTML5
+CSS3
+GIT
+EASYEDA
+SOLDERING
+Links:
+READ MORE →
+click to zoom
+RC Car Lighting System
+Hobby - August 2017
+
+With the help of an Arduinos I read out the throttle control of the RC receiver and was able to control the brake light accordingly. Additionally I developed circuit boards for the LEDs to mount them easily.
+
+Used Tools:
+ARDUINO
+SOLDERING
+KICAD
+Links:
+READ MORE →
+click to zoom
+FastFeed
+Hobby - May 2017
+
+Mobile Internet is really expensive in Germany. I developed FastFeed to be able to read news even when the reception is poor. A super small webapp, which causes only minimal traffic.
+
+Used Tools:
+HTML5
+CSS3
+JAVASCRIPT
+SERVICEWORKER
+Links:
+click to zoom
+CleanIT WordPress Theme
+Hobby - March 2017
+
+My first own Wordpress theme. The focus of the development was on performance, minimalism and extensibility. To this day, I use it in my blog to ensure optimal loading times.
+
+Used Tools:
+WORDPRESS
+HTML5
+CSS3
+JAVASCRIPT
+PHP
+Links:
+READ MORE →
+click to zoom
+\\"Quissen\\"-App
+Work - December 2016
+
+For the company Lightball SSC I finished the development of the quiz app \\"Quissen\\". It is using Angular 1 with an Wordpress REST Backend.
+
+Used Tools:
+JAVASCRIPT
+WORDPRESS
+HTML5
+CSS3
+GIT
+ANGULAR 1
+IONIC
+Links:
+click to zoom
+Websnooze V1
+Hobby - August 2016
+
+Having multiple monitors at your desktop is awesome, but sometimes you aren't using it. Why not showing some calm images on it to keep you relaxed and balanced on work. Websnooze was an application that showed you images from unsplash.com in your new tab page or on a dedicated webpage.
+
+Used Tools:
+JAVASCRIPT
+HTML5
+CSS3
+SERVICEWORKER
+CHROME EXTENSION
+click to zoom
+Open Gallery
+Hobby - July 2016
+
+Photosharing and Collecting made easy.
+
+Used Tools:
+PHP
+JAVASCRIPT
+CSS3
+HTML5
+SQL
+SQLITE
+Links:
+click to zoom
+Stepcraft CNC GRBL Controler
+Hobby - May 2016
+
+Our school got an awesome Stepcraft CNC but the software was horrible. Sadly the used controller was proprietary. The solution: I wired up an arduino (with a lot of reverse engineering), uploaded some software. We can now use any public available GRBL Software like the one provieded by easel.com. 
+
+Used Tools:
+ARDUINO
+CNC
+EASEL
+SOLDERING
+Links:
+READ MORE →
+click to zoom
+New Theme For School Library Homepage
+Hobby - April 2016
+
+A friend of me programmed a book inventory system for our school library, but the UI was pretty ugly (sorry my friend). So we came together and I updated the css files without changing the HTML structure as best as possible to make it more appealing to the eye.
+
+Used Tools:
+CSS3
+Links:
+click to zoom
+LED Coaster
+Hobby - April 2016
+
+We had an CNC machine, and I had some freetime and a lot of ideas. One of those was an cool coaster concept that doubled as a lamp at the late afternoon BBQ partys. The Coaster illuminates your drink and you get some colorfull light at your party while still be able to see your friends.
+
+Used Tools:
+CNC
+EASEL
+SOLDERING
+click to zoom
+School Information Telegram Bot
+Hobby - November 2015
+
+As a student I was tired of actively searching for new substitution lessons or school news. So I developed a telegram bot that can inform me about these things. @archenhold_bot
+
+Used Tools:
+TELEGRAM
+PYTHON
+PHP
+Links:
+click to zoom
+VisualAdvise
+Work - July 2015
+
+In a design competition of the company \\"Visual Advise\\" I could convince and was allowed to design the factsheet of the company.
+
+Used Tools:
+GIMP
+Links:
+READ MORE →
+click to zoom
+School App
+Hobby - April 2015
+
+For our school I developed an app, which could manage the most important data for students, such as substitutions, study plans and homework.
+
+Used Tools:
+THUNKABLE APP BUILDER
+PHP
+MIT APP INVENTOR
+Links:
+READ MORE →
+click to zoom
+Substitution Plan Upload System
+Hobby - March 2015
+
+My school uses an custom made software for displying and managing substitutions. I extended this software with an automatic upload script to our school server. We can now view our substitions online on our website.
+
+Used Tools:
+HTML5
+CSS3
+JAVASCRIPT
+Links:
+READ MORE →
+click to zoom
+Mobile Friendly Theme For School Homepage
+Hobby - June 2014
+
+At our school we had the privilege to have full control over our website, unfortunately it was not mobile friendly at that time. So my first real web development project was to make this site mobile and friendly. The architecture isn't great, but it works with solid performance thanks to clever caching techniques.
+
+Used Tools:
+PHP
+HTML5
+JAVASCRIPT
+CSS3
+Links:
+click to zoom
+Robocup World Championship Eindhoven
+Hobby - June 2013
+
+After the success at the German Championship in the Robocup Junior, my team and I were allowed to participate at the World Championship in Eindhoven. Unfortunately this year we were not as successful as last year. Nevertheless it was a great experience and we had a lot of fun.
+
+Used Tools:
+ARDUINO
+SOLDERING
+BRICXCC
+KICAD
+click to zoom
+Robocup 2013 - German Champion
+Hobby - March 2013
+
+After successful participation at the World Championships 2012, my team and I finally managed to become German Champion in the Robocup Junior Competition in 2013.
+
+Used Tools:
+ARDUINO
+KICAD
+BRIXCC
+SOLDERING
+click to zoom
+Robocup 2012 - World Champion
+Hobby - June 2012
+
+After successful qualification both at the Berlin and the German Championships, my team and I won a competition at the Robocup Junior World Championship 2012 in Mexico City. So I can proudly call myself a world champion . The task was to build and program a line following robot that also had the ability to locate and rescue a victim within a room.
+
+Used Tools:
+ARDUINO
+KICAD
+BRICXCC
+SOLDERING
+Links:
+About Me "
+`;
