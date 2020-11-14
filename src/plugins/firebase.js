4644585c014afa51154f5e27620ec84829d8b628
Adrian Jost <22987140+adrianjost@users.noreply.github.com>
2020-11-14 09:32:01 +0100

    update firebase config

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -3,13 +3,13 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const config = {
-	apiKey: "AIzaSyDlyFP3-bIBwQ_JcvlaPkJD88WoEUlF9eg",
+	apiKey: "AIzaSyBC8UNa0JZAXq0IYO-LjH3aUT8_2OROnhs",
 	authDomain: "curriculum-vitae-5cd0a.firebaseapp.com",
 	databaseURL: "https://curriculum-vitae-5cd0a.firebaseio.com",
 	projectId: "curriculum-vitae-5cd0a",
 	storageBucket: "curriculum-vitae-5cd0a.appspot.com",
 	messagingSenderId: "708068057486",
-	appId: "1:708068057486:web:a1dff7d0bd7af2da",
+	appId: "1:708068057486:web:a1dff7d0bd7af2da"
 };
 
 // initialize if not already done
